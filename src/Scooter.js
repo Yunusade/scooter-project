b66b1b7c5336e8b552e8845612d0375801db2792
Yunusade <146001543+Yunusade@users.noreply.github.com>
2023-10-27 10:57:52 +0100

    First commit

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,51 @@
 class Scooter{
   // scooter code here
+  static nextSerial = 1
+  constructor(station){
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 0
+    this.isBroken = false
+  }
+
+  rent(user) {
+    if(this.charge > 20 && !this.isBroken){
+      this.station = null
+      this.user = user
+    }else{
+      throw new Error("scooter needs to charge or scooter needs repair")
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null
+  }
+  
+  async recharge() {
+    let difference = 100 - this.charge;
+    if(difference === 0) throw new Error("Scooter is already 100%")
+
+    for(let i = 0; i < difference; i++) {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.charge = this.charge + 1
+
+      if(this.charge % 3 === 0) console.log(`Scooter is charged ${this.charge}%`)
+    }
+  }
+
+  requestRepair() {
+    if(this.isBroken !== true){
+      throw new Error("Your scooter doesn't need a repair")
+    }
+
+    setTimeout(() => {
+      this.isBroken = false
+      console.log("Repair Completed")
+    }, 5000);
+  }
+
 }
 
 
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,100 @@
-const User = require('./User')
-const Scooter = require('./Scooter')
+const User = require("./User");
+const Scooter = require("./Scooter");
 
 class ScooterApp {
+  constructor() {
+    this.stations = { "Station A": [], "Station B": [], "Station C": [] };
+    this.registeredUsers = {};
+  }
+
+  registerUser(username, password, age) {
+    if (!this.registeredUsers.hasOwnProperty(username) && age >= 18) {
+      const newUser = new User(username, password, age);
+      this.registeredUsers[username] = newUser;
+      console.log("user has been registered");
+      return newUser;
+    } else if (age < 18) {
+      throw new Error("too young to register");
+    } else {
+      throw new Error("already registered");
+    }
+  }
+
+  loginUser(username, password) {
+    if (this.registeredUsers.hasOwnProperty(username)) {
+      const user = this.registeredUsers[username];
+
+      if (user.password === password) {
+        console.log("user has been logged in.");
+      } else {
+        throw new Error("Username or password is incorrect.");
+      }
+    } else {
+      throw new Error("Username or password is incorrect.");
+    }
+  }
+
+  logoutUser(username) {
+    if (this.registeredUsers.hasOwnProperty(username)) {
+      console.log("user is logged out");
+    } else {
+      throw new Error("no such user is logged in");
+    }
+  }
+
+  createScooter(station) {
+    if (this.stations.hasOwnProperty(station)) {
+      const newScooter = new Scooter(station);
+      this.stations[station].push(newScooter);
+      console.log("created new scooter");
+      return newScooter;
+    } else {
+      throw new Error("no such station error");
+    }
+  }
+
+  dockScooter(scooter, station) {
+
+    if(this.stations[station].includes(scooter)){
+      throw new Error("scooter already at station")
+    }
+
+    if (this.stations.hasOwnProperty(station)) {
+      this.stations[station].push(scooter);
+      scooter.dock(station);
+      console.log("scooter is docked");
+    }else{
+      throw new Error("no such station error");
+    }
+
+  }
+
+  rentScooter(scooter, user) {
+    for (const station in this.stations) {
+        const stationScooters = this.stations[station];
+        const scooterIndex = stationScooters.indexOf(scooter);
+        if (scooterIndex !== -1) {
+          if (scooter.user === null) {
+              stationScooters.splice(scooterIndex, 1);
+              scooter.rent(user);
+              console.log(`${user.username} has rented a scooter from ${station}`);
+              return;
+          } else {
+              throw new Error(`Scooter already rented by ${scooter.user.username}`);
+          }
+        }
+    }
+  }
+
+  print() {
+    console.log(this.registeredUsers())
+    console.log(this.stations())
+    for(const station in this.stations){
+      console.log(`station has ${station.length} scooters`)
+    }
+  }
+
   // ScooterApp code here
 }
 
-module.exports = ScooterApp
+module.exports = ScooterApp;
